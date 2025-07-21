162d2cdaad9fb26572ef9f3a9d5de68534642578
ni <1438133057@qq>
2025-07-21 02:10:30 -0700

    definy view

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.http import HttpRequest
+
+
+def index(request):
+
+    return HttpResponse('OK')
