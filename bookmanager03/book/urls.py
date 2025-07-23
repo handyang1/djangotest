55b3c84d4c4ee8c7c18c709f5653192ad273359e
ni <1438133057@qq>
2025-07-22 21:39:29 -0700

    order get

--- bookmanager03/book/urls.py
+++ bookmanager03/book/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from book.views import create_book
+from book.views import create_book, shop
 
 urlpatterns=[
     path('create/', create_book),
+    # path('<city_id>/<shop_id>/', shop),
+    path('<city_id>/<shop_id>/',shop)
 ]
--- bookmanager03/book/views.py
+++ bookmanager03/book/views.py
@@ -5,10 +5,33 @@ from book.models import BookInfo
 
 # Create your views here.
 def create_book(request):
-
-    book=BookInfo.objects.create(
+    book = BookInfo.objects.create(
         name='abc',
         pub_date='2000-1-1',
         readcount=10
     )
     return HttpResponse('create')
+
+
+# def shop(request, city_id, shop_id):
+#     #print(city_id, shop_id)
+#
+#     query_params = request.GET
+#     print(query_params)
+#     order = query_params.getlist('order')
+#
+#     print(order)
+#     # <QueryDict: {'order': ['readcount']}>
+#     return HttpResponse('boom boom boom!!!!!')
+
+
+def shop(request,city_id,shop_id):
+    print(city_id,shop_id)
+
+
+    query_prams = request.GET
+    print(query_prams)
+
+    order = query_prams.getlist('order')
+    print(order)
+    return HttpResponse('aaaaaaaaaaa')
